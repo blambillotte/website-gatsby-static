b7f79ee74e67a71f438344bf95cc93a2d4003a8e
Ben Lambillotte <ben.lambillotte@gmail.com>
2019-12-14 20:11:43 -0800

    Updates

--- src/components/ContentContainer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ContentContainer = (props) => (
-  <div className="content-container swing-up">
-    {props.content}
-  </div>
-);
-
-export default ContentContainer;
