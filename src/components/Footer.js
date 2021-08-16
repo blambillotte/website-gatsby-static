31e487a788ff2ebb33b5c1aa16c382cdb98847f5
Ben Lambillotte <ben.lambillotte@gmail.com>
2021-08-15 19:34:26 -0700

    Add ingredient convertor to portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,17 +7,7 @@ const getYear = () => {
 
 const Footer = () => (
   <div className="footer">
-    <p>
-      Built with{' '}
-      <span role="img" aria-label="heart">
-        ❤️
-      </span>{' '}
-      and{' '}
-      <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
-        React
-      </a>{' '}
-      by Ben Lambillote ⓒ {getYear()}
-    </p>
+    <span>Ben Lambillotte ⓒ {getYear()}</span>
   </div>
 );
 
